5b3e2268d022330918c5bdf940d75fbf011efa86
李子龙 <15131968@qq.com>
2015-10-21 12:16:40 +0800

    commit util

---
+++ EaseUI/EaseUI/EaseUI.h
@@ -0,0 +1,45 @@
+//
+//  EaseUI.h
+//  EaseUI
+//
+//  Created by EaseMob on 15/9/22.
+//  Copyright (c) 2015年 easemob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EaseUI.
+FOUNDATION_EXPORT double EaseUIVersionNumber;
+
+//! Project version string for EaseUI.
+FOUNDATION_EXPORT const unsigned char EaseUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EaseUI/PublicHeader.h>
+
+#import <EaseUI/EaseConversationListViewController.h>
+#import <EaseUI/EaseMessageViewController.h>
+#import <EaseUI/EaseUsersListViewController.h>
+#import <EaseUI/EaseViewController.h>
+
+#import <EaseUI/IModelCell.h>
+#import <EaseUI/IModelChatCell.h>
+#import <EaseUI/EaseMessageCell.h>
+#import <EaseUI/EaseBaseMessageCell.h>
+#import <EaseUI/EaseBubbleView.h>
+#import <EaseUI/EaseUserCell.h>
+
+
+#import <EaseUI/EaseChineseToPinyin.h>
+#import <EaseUI/EaseEmoji.h>
+#import <EaseUI/EaseEmotionEscape.h>
+#import <EaseUI/EaseEmotionManager.h>
+#import <EaseUI/EaseSDKHelper.h>
+#import <EaseUI/EMCDDeviceManager.h>
+#import <EaseUI/EaseConvertToCommonEmoticonsHelper.h>
+
+#import <EaseUI/NSDate+Category.h>
+#import <EaseUI/NSObject+EaseMob.h>
+#import <EaseUI/NSString+Valid.h>
+#import <EaseUI/UIImageView+EMWebCache.h>
+#import <EaseUI/UIViewController+HUD.h>
+#import <EaseUI/UIViewController+DismissKeyboard.h>
