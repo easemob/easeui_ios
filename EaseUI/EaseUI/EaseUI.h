c1b314ccccae933f2660667d29cd0ac8e55be500
李子龙 <15131968@qq.com>
2015-10-26 10:59:04 +0800

    update:remove pod from project

--- EaseUI/EaseUI/EaseUI.h
+++ EaseUI/EaseUI/EaseUI.h
@@ -2,44 +2,40 @@
 //  EaseUI.h
 //  EaseUI
 //
-//  Created by EaseMob on 15/9/22.
+//  Created by EaseMob on 15/10/22.
 //  Copyright (c) 2015年 easemob. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for EaseUI.
-FOUNDATION_EXPORT double EaseUIVersionNumber;
+#import "EaseConversationListViewController.h"
+#import "EaseMessageViewController.h"
+#import "EaseUsersListViewController.h"
+#import "EaseViewController.h"
 
-//! Project version string for EaseUI.
-FOUNDATION_EXPORT const unsigned char EaseUIVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <EaseUI/PublicHeader.h>
-
-#import <EaseUI/EaseConversationListViewController.h>
-#import <EaseUI/EaseMessageViewController.h>
-#import <EaseUI/EaseUsersListViewController.h>
-#import <EaseUI/EaseViewController.h>
-
-#import <EaseUI/IModelCell.h>
-#import <EaseUI/IModelChatCell.h>
-#import <EaseUI/EaseMessageCell.h>
-#import <EaseUI/EaseBaseMessageCell.h>
-#import <EaseUI/EaseBubbleView.h>
-#import <EaseUI/EaseUserCell.h>
+#import "IModelCell.h"
+#import "IModelChatCell.h"
+#import "EaseMessageCell.h"
+#import "EaseBaseMessageCell.h"
+#import "EaseBubbleView.h"
+#import "EaseUserCell.h"
 
 
-#import <EaseUI/EaseChineseToPinyin.h>
-#import <EaseUI/EaseEmoji.h>
-#import <EaseUI/EaseEmotionEscape.h>
-#import <EaseUI/EaseEmotionManager.h>
-#import <EaseUI/EaseSDKHelper.h>
-#import <EaseUI/EMCDDeviceManager.h>
-#import <EaseUI/EaseConvertToCommonEmoticonsHelper.h>
+#import "EaseChineseToPinyin.h"
+#import "EaseEmoji.h"
+#import "EaseEmotionEscape.h"
+#import "EaseEmotionManager.h"
+#import "EaseSDKHelper.h"
+#import "EMCDDeviceManager.h"
+#import "EaseConvertToCommonEmoticonsHelper.h"
 
-#import <EaseUI/NSDate+Category.h>
-#import <EaseUI/NSObject+EaseMob.h>
-#import <EaseUI/NSString+Valid.h>
-#import <EaseUI/UIImageView+EMWebCache.h>
-#import <EaseUI/UIViewController+HUD.h>
-#import <EaseUI/UIViewController+DismissKeyboard.h>
+#import "NSDate+Category.h"
+#import "NSObject+EaseMob.h"
+#import "NSString+Valid.h"
+#import "UIImageView+EMWebCache.h"
+#import "UIViewController+HUD.h"
+#import "UIViewController+DismissKeyboard.h"
+
+@interface EaseUI : NSObject
+
+@end
