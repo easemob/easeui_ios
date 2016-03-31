fe2b196cdf3cb67187449e375e90b66cde117b7a
lizilong1989 <15131968@qq.com>
2016-03-31 22:57:45 +0800

    update copyright

--- EaseUI/EaseUI/EMUIKit/Util/FixFopen.c
+++ EaseUI/EaseUI/EMUIKit/Util/FixFopen.c
@@ -1,10 +1,14 @@
-//
-//  FixFopen.c
-//  ChatDemo-UI3.0
-//
-//  Created by EaseMob on 15/10/29.
-//  Copyright (c) 2015年 EaseMob. All rights reserved.
-//
+/************************************************************
+ *  * Hyphenate CONFIDENTIAL
+ * __________________
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of Hyphenate Inc.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Hyphenate Inc.
+ */
 
 #include <stdio.h>
 #include <unistd.h>
