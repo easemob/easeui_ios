e1743c6ef8a105eae54f9a48735558383650fb04
wyuzhang <sxqxdong@sina.com>
2016-12-22 07:51:16 +0800

    utils add notes

--- EaseUI/EaseUI/EMUIKit/Util/EaseConvertToCommonEmoticonsHelper.h
+++ EaseUI/EaseUI/EMUIKit/Util/EaseConvertToCommonEmoticonsHelper.h
@@ -14,8 +14,22 @@
 
 @interface EaseConvertToCommonEmoticonsHelper : NSObject
 
+/*!
+ @method
+ @brief 系统emoji表情转换为表情编码
+ @discussion
+ @param text   待转换的文字
+ @return  转换后的文字
+ */
 + (NSString *)convertToCommonEmoticons:(NSString *)text;
 
+/*!
+ @method
+ @brief 表情编码转换为系统emoji表情
+ @discussion
+ @param text   待转换的文字
+ @return  转换后的文字
+ */
 + (NSString *)convertToSystemEmoticons:(NSString *)text;
 
 @end
