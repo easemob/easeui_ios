90c43bcf49a9a539f18891bce4f009e836ac04e9
wyuzhang <sxqxdong@sina.com>
2016-12-21 19:36:59 +0800

    models add notes

--- EaseUI/EaseUI/EMUIKit/Model/IModelCell.h
+++ EaseUI/EaseUI/EMUIKit/Model/IModelCell.h
@@ -12,18 +12,41 @@
 
 #import <Foundation/Foundation.h>
 
+/** @brief 消息cell的协议 */
+
 @protocol IModelCell <NSObject>
 
 @required
 
+/** @brief 消息对象 */
 @property (strong, nonatomic) id model;
 
+/*!
+ @method
+ @brief 获取cell的reuseIdentifier
+ @param model    消息对象模型
+ @return reuseIdentifier
+ */
 + (NSString *)cellIdentifierWithModel:(id)model;
 
+/*!
+ @method
+ @brief 获取cell的高度
+ @param model    消息对象模型
+ @return cell的高度
+ */
 + (CGFloat)cellHeightWithModel:(id)model;
 
 @optional
 
+/*!
+ @method
+ @brief 初始化消息cell
+ @param style              cell的样式
+ @param reuseIdentifier    cell的重用标识符
+ @param model              消息对象
+ @return UITableViewCell
+ */
 - (instancetype)initWithStyle:(UITableViewCellStyle)style
               reuseIdentifier:(NSString *)reuseIdentifier
                         model:(id)model;
