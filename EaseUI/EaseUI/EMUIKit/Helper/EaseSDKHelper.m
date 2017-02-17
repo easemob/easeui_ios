4e16c6539c9c4f2df54c6ac2b4a21c820fe219d6
lizilong1989 <15131968@qq.com>
2017-02-17 12:24:48 +0800

    Add a switch for httpsonly in setting

--- EaseUI/EaseUI/EMUIKit/Helper/EaseSDKHelper.m
+++ EaseUI/EaseUI/EMUIKit/Helper/EaseSDKHelper.m
@@ -217,6 +217,10 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
         flag = [[otherConfig objectForKey:@"easeSandBox"] boolValue];
     }
     if (!flag) {
+        NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+        NSNumber *httpsOnly = [ud objectForKey:@"identifier_httpsonly_enable"];
+        [[EaseMob sharedInstance].chatManager setIsUseHttpsOnly:[httpsOnly boolValue]];
+        
         //注册easemob sdk
         [[EaseMob sharedInstance] registerSDKWithAppKey:appkey
                                            apnsCertName:apnsCertName
