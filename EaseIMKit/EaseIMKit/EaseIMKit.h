65d434ae44541ac9aa5d028c82034eb7ea0d886b
dujiepeng <347302029@qq.com>
2020-10-29 11:30:35 +0800

    create project

---
+++ EaseIMKit/EaseIMKit/EaseIMKit.h
@@ -0,0 +1,19 @@
+//
+//  EaseIMKit.h
+//  EaseIMKit
+//
+//  Created by 杜洁鹏 on 2020/10/29.
+//  Copyright © 2020 djp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EaseIMKit.
+FOUNDATION_EXPORT double EaseIMKitVersionNumber;
+
+//! Project version string for EaseIMKit.
+FOUNDATION_EXPORT const unsigned char EaseIMKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EaseIMKit/PublicHeader.h>
+
+
