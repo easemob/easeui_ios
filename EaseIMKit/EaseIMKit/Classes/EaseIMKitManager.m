77b786736c919a5ff940983d1d2a48c7473e31a6
zhangchong <chongzhang@easemob.com>
2021-10-22 16:14:12 +0800

    udpate EaseIMKit version

--- EaseIMKit/EaseIMKit/Classes/EaseIMKitManager.m
+++ EaseIMKit/EaseIMKit/Classes/EaseIMKitManager.m
@@ -13,7 +13,7 @@
    
 bool gInit;
 static EaseIMKitManager *easeIMKit = nil;
-static NSString *g_UIKitVersion = @"3.8.5";
+static NSString *g_UIKitVersion = @"3.8.7";
 
 @interface EaseIMKitManager ()<EMMultiDevicesDelegate, EMContactManagerDelegate, EMGroupManagerDelegate, EMChatManagerDelegate>
 @property (nonatomic, strong) EaseMulticastDelegate<EaseIMKitManagerDelegate> *delegates;
@@ -22,7 +22,7 @@ static NSString *g_UIKitVersion = @"3.8.5";
 @property (nonatomic, strong) dispatch_queue_t msgQueue;
 @end
 
-#define IMKitVersion @"3.8.5"
+#define IMKitVersion @"3.8.7"
 
 @implementation EaseIMKitManager
 + (BOOL)initWithEMOptions:(EMOptions *)options {
